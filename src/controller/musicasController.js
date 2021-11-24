007de6b7aebff5f87a069150564f07c4ab38d5c3
ruaanlucass <lruan9440@gmail.com>
2021-11-23 21:15:44 -0300

    Atualizando routes e controller

--- src/controller/musicasController.js
+++ src/controller/musicasController.js
@@ -1,10 +1,26 @@
 const musicas = require("../models/musicas.json")
+const fs = require("fs")
 
 const getAllMusicas = (req, res) => {
     console.log(req.url);
     res.status(200).send(musicas)
 }
 
+const createMusicas = (req, res) => {
+    const { id, title, duration, launchYear, favorited, artists } = req.body
+    musicas.push({ id, title, duration, launchYear, favorited, artists })
+    fs.writeFile("./src/models/musicas.json", JSON.stringify(musicas), 'utf8', function (err) {
+        if (err) {
+            res.status(500).send({ message: err })
+        } else {
+            console.log("Arquivo atualizado com sucesso!")
+            const musicasFound = musicas.find(musicas => musicas.id == id)     
+            res.status(200).send(musicasFound)
+        }
+    })
+}
+
 module.exports = {
+    createMusicas,
     getAllMusicas,
 }
--- src/routes/musicasRoutes.js
+++ src/routes/musicasRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controller/musicasController')
 
 router.get("/", controller.getAllMusicas);
+router.post("/", controller.createMusicas)
 
 module.exports = router;
