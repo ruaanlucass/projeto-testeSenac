bc4845f660ec6dc1a8756d829da5bec554f0a38a
ruaanlucass <lruan9440@gmail.com>
2021-11-26 21:13:04 -0300

    API pronta

--- src/controller/musicasController.js
+++ src/controller/musicasController.js
@@ -40,9 +40,95 @@ const getMusicasByLaunchYear = (req, res) => {
     }
 }
 
+const updateMusicas = (req, res) => {
+    try {
+        const musicasId = req.params.id
+        const musicasToUpdate = req.body 
+
+        const musicasFound = musicas.find(musicas => musicas.id == musicasId)
+        const musicasIndex = musicas.indexOf(musicasFound) 
+
+        if (musicasIndex >= 0) { 
+            musicas.splice(musicasIndex, 1, musicasToUpdate) 
+        } else {
+            res.status(404).send({ message: "Filme não encontrado para ser atualizado" })
+        }
+
+        fs.writeFile("./src/models/musicas.json", JSON.stringify(musicas), 'utf8', function (err) { 
+            if (err) {
+                res.status(500).send({ message: err }) 
+            } else {
+                console.log("Arquivo de filmes atualizado com sucesso!")
+                const musicasUpdated = musicas.find(musicas => musicas.id == musicasId) 
+                res.status(200).send(musicasUpdated) 
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err }) 
+    }
+}
+
+const updateFavoritedStatus = (req, res) => {
+    try {
+        const musicasId = req.params.id // pego a informação do id no parametro da requisição
+        const favorited = req.body.favorited // pego a informação de watched no corpo da requisição. Ele terá valor true ou false, dependendo do que tiver sido passado
+
+        const musicasToUpdate = musicas.find(musicas => musicas.id == musicasId) // separo o filme que irei mudar o status
+        const musicasIndex = musicas.indexOf(musicasToUpdate) // identifico o índice do filme no meu array
+
+        if (musicasIndex >= 0) { // verifico se o filme existe no array de filmes
+            musicasToUpdate.favorited = favorited //atualizo o objeto com o novo status informando se foi assistido ou não
+            musicas.splice(musicasIndex, 1, musicasToUpdate) // removo o filme pelo índice substituindo pelo novo
+        } else {
+            res.status(404).send({ message: "Musica não encontrado para informar se foi assistido ou não" })
+        }
+
+        fs.writeFile("./src/models/musicas.json", JSON.stringify(musicas), 'utf8', function (err) { // gravo meu json de filmes atualizado
+            if (err) {
+                res.status(500).send({ message: err })
+            } else {
+                console.log("Arquivo atualizado com sucesso!")
+                const musicasUpdated = musicas.find((musicas) => musicas.id == musicasId) // separo o filme que modifiquei no array
+                res.status(200).send(musicasUpdated) // envio o filme modificado como resposta
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err })
+    }
+}
+
+const deleteMusicas = (req, res) => {
+    try {
+        const musicasId = req.params.id
+        const musicasFound = musicas.find(musicas => musicas.id == musicasId) // encontro o filme pelo id
+        const musicasIndex = musicas.indexOf(musicasFound) // identifico o índice do filme no meu array
+
+        if (musicasIndex >= 0) { // verifico se o filme existe no array de filmes
+            musicas.splice(musicasIndex, 1) // removo o filme pelo índice
+        } else {
+            res.status(404).send({ message: "Musica não encontrado para ser deletado" })
+        }
+
+        fs.writeFile("./src/models/musicas.json", JSON.stringify(musicas), 'utf8', function (err) { // gravo meu array de filmes sem o filme que deletei
+            if (err) {
+                res.status(500).send({ message: err })
+            } else {
+                console.log("Musica deletado com sucesso do arquivo!")
+                res.sendStatus(204)
+            }
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Erro ao deletar o filme" })
+    }
+}
+
 module.exports = {
-    getMusicas,
     createMusicas,
+    deleteMusicas,
+    updateMusicas,
+    updateFavoritedStatus,
+    getMusicas,
     getAllMusicas,
     getMusicasByLaunchYear,
 }
--- src/routes/musicasRoutes.js
+++ src/routes/musicasRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const controller = require('../controller/musicasController')
 
 router.get("/", controller.getAllMusicas);
-router.post("/", controller.createMusicas)
-/*router.get("/:id", controller.getMusicas)*/
-router.get("/:launchYear", controller.getMusicasByLaunchYear)
+router.post("/", controller.createMusicas);
+router.get("/:id", controller.getMusicas);
+router.get("/:launchYear", controller.getMusicasByLaunchYear);
+router.put("/:id", controller.updateMusicas);
+router.patch("/:id/favorited", controller.updateFavoritedStatus);
+router.delete("/:id", controller.deleteMusicas);
 
 module.exports = router;
